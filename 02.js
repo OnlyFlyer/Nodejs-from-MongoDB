328ef4669c9ce2886107dcde2de3a5175429f6c4
OnlyFlyer <m15776849661@163.com>
2016-12-02 21:54:34 +0800

    更新

--- 02.js
+++ 02.js
@@ -2,12 +2,31 @@ var express = require("express");
 var app = express();
 var db = require("./model/db.js");
 app.get("/",function(req,res){
-    db.insertOne('student',{'name':'xiaowu','age':29,'sex':'nv'},function(err,result){
+
+    // db.insertOne('student',{'name':'xiaozhang','age':parseInt(Math.random() * 100),'sex':'nv'},function(err,result){
+    //     if(err){
+    //         console.log('插入失败!');
+    //     }
+    //     console.log('插入成功');
+    //     res.send(result);
+    // });
+
+    // db.find('student',{'name':'xiaozhang'},function(err,result){
+    //     if(err){
+    //         console.log('查找失败!');
+    //         return;
+    //     }
+    //     res.send(result);
+    //     // console.log(result);
+    // });
+
+    db.updateOne('student',{'sex':'nv',$set:'nan'}, function(err,result){
         if(err){
-            console.log('插入失败!');
+            console.log("更新失败");
+            return;
         }
-        console.log('插入成功');
         res.send(result);
+        console.log(result);
     });
     });
 app.listen(3000);
