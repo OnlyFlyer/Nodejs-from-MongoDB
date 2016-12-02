d7bb57a8bf0a674d903736fa9dc6865b9ef9baa3
OnlyFlyer <m15776849661@163.com>
2016-12-02 21:58:35 +0800

    更新

--- model/db.js
+++ model/db.js
@@ -1,27 +1,83 @@
 var MongoClient = require('mongodb').MongoClient;
+
 //封装数据库常用增删改查等函数
+
 //内部函数 连接数据库
-function _connectDB(callback){
-	var url = 'mongodb://localhost:27017/haha';
-	//连接数据库
-	MongoClient.connect(url, function(err,db){
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+
+function _connect(callback){
+	// Use connect method to connect to the server
+	MongoClient.connect(url, function(err, db) {
 		callback(err,db);
 	});
 }
-// _connectDB();
+// _connect();
+
 
 //插入数据
 exports.insertOne = function(collectionName, json, callback){
-	_connectDB(function(err,db){
+	_connect(function(err,db){
 		if(err){
-			console.log('连接失败！');
+			console.log('数据库连接失败!');
+			return;
 		}
-		db.collections(collectionName).insertOne(json, function(err,result){
+		db.collection(collectionName).insertOne(json, function(err,result){
 			callback(err,result);
 			db.close();
 		});
 	});
 };
+// insertOne('student',{'name':'xiaoliu','age':29,'sex':'nv'},function(err,result){
+// 	if(err){
+// 		console.log("插入失败");
+// 		return;
+// 	}
+// 	console.log();
+// });
+
+exports.find = function(collectionName, json, callback){
+	// var result = [];
+	_connect(function(err,db){
+		if(err){
+			console.log("连接数据库失败");
+			return;
+		}
+		db.collection(collectionName).find(json).toArray(function(err,result){
+			callback(err,result);
+		});
+	});
+};
+// find('student',{'name':'xiaozhang'},function(err,result){
+// 	if(err){
+// 		console.log("查找失败!");
+// 		return;
+// 	}
+// 	console.log(result);
+// });
+
+
+
+//更新数据
+exports.updateOne = function(collectionName, json, callback){
+	_connect(function(err,db){
+		if(err){
+			console.log('数据库连接失败!');
+			return;
+		}
+		db.collection(collectionName).updateOne(json, function(err,result){
+			callback(err,result);
+		});
+	});
+};
+// update('student',{'sex':'nv',$set:{'sex':'nan'}},function(err,result){
+// 	if(err){
+// 		console.log("更新失败!");
+// 		return;
+// 	}
+// 	console.log('chenggong');
+// 	console.log(result);
+// });
 
 
 
@@ -74,6 +130,89 @@ exports.insertOne = function(collectionName, json, callback){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function _connectDB(callback){
+// 	var url = 'mongodb://localhost:27017/haha';
+// 	//连接数据库
+// 	MongoClient.connect(url, function(err,db){
+// 		callback(err,db);
+// 	});
+// }
+// // _connectDB();
+//
+// //插入数据
+// exports.insertOne = function(collectionName, json, callback){
+// 	_connectDB(function(err,db){
+// 		if(err){
+// 			console.log('连接失败！');
+// 		}
+// 		db.collections(collectionName).insertOne(json, function(err,result){
+// 			callback(err,result);
+// 			db.close();
+// 		});
+// 	});
+// };
 
 // function _connectDB(callback){
 // 	var url = 'mongodb://localhost:27017/myproject';
